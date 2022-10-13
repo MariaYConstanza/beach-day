2666fd1eccc0e16e68652c36bb5f3888f095fa96
Tab-Y <tlyoumans@gmail.com>
2022-10-13 14:45:59 -0400

    fetch function test

---
+++ develop/js/script.js
@@ -0,0 +1,21 @@
+
+
+// test function for checking weather API
+function check(){
+    fetch("https://api.weather.gov/points/38.8894,-77.0352")
+    .then(function (response){
+        return response.json();
+    })
+    .then (function (data) {
+        console.log(data.properties.forecast);
+        fetch(data.properties.forecast)
+        .then(function (resposne2){
+            return resposne2.json();
+        })
+        .then(function (data2) {
+            console.log(data2)
+        })}
+    )
+}
+
+check();
