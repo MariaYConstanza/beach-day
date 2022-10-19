d5d02548ae49dcef4eeb475b72fd663269b9f497
Mary Elenius <mary.panda.jackson@gmail.com>
2022-10-19 02:49:49 -0400

    Tides info in console log

--- assets/js/tides.js
+++ assets/js/tides.js
@@ -1,9 +1,35 @@
 // api key 9878347f-b060-480e-b198-7c2bf869297d
 // endpoint http://www.worldtides.info/api/v3?
 
+// DOM
+var tideContainer = document.getElementById("futureTides");
+
+// Geolocation API
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    tideContainer.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+// Shows position and defines long and lat
+function showPosition(position) {
+  tideContainer.innerHTML =
+    "Latitude: " +
+    position.coords.latitude +
+    "<br>Longitude: " +
+    position.coords.longitude;
+  console.log(position);
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+}
+
+// Button for tides
 var generateBtn = document.getElementById("displayTides");
 generateBtn.addEventListener("click", getTides);
 
+// API fetch
 const optionsTides = {
   method: "GET",
   headers: {
@@ -13,19 +39,23 @@ const optionsTides = {
 
 function getTides(event) {
   event.preventDefault();
-
   fetch(
-    "http://www.worldtides.info/api/v3?heights&extremes&date={date}&lat={lat}&lon={lon}&days=7&key=9878347f-b060-480e-b198-7c2bf869297d",
+    "http://www.worldtides.info/api/v3?heights&extremes&days=7&lat=27.34&lon=-80.31&key=9878347f-b060-480e-b198-7c2bf869297d",
     optionsTides
   )
     .then((response) => response.json())
     .then((response) => {
-      //   var tideContainer = document.getElementById("");
-      //   var tideElement = document.getElementById("");
-      //   console.log(tideElement);
       console.log(response);
-      //   tideElement.innerHTML =
-      //   tideContainer.appendChild(tideElement);
     })
-    .catch((err) => console.error(err));
+    .catch(console.error);
+}
+
+// Function to render data into HTML
+function displayTideForecast(response) {
+  const tides = [0];
+  const displayTideForecast = document.getElementById("futureTides");
+  const tides1 = test;
+  const heading = document.createElement("h1");
+  heading.innerHTML = heading;
+  displayTideForecast.appendChild(heading);
 }
